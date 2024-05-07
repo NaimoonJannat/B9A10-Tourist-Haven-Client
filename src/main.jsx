a41d559b4c766fd6948188c44bbd18e646b4a47c
NaimoonJannat <naimoonjannat@gmail.com>
2024-05-07 11:53:36 +0600

    Error page added

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,27 @@ import {
  } from "react-router-dom"; 
 import Root from './component/Root';
 import Home from './component/Home';
+import ErrorPage from './component/ErrorPage';
+import Login from './component/Login';
+import Register from './component/Register';
 
  const router = createBrowserRouter([ 
   { 
   path: "/", 
   element: <Root></Root>, 
+  errorElement:<ErrorPage></ErrorPage>,
   children: [
     {
       path:'/',
       element:<Home></Home>
     },
     {
-      
+      path:'/login',
+      element:<Login></Login>,
+    },
+    {
+      path:'/register',
+      element:<Register></Register>,
     }
   ]
   }, 
